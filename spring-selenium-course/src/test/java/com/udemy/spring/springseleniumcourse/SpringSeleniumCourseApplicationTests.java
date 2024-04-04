0d1874904c764b6e7cca960d2f7dc4facc73110d
AndresHGdeG <a.h.mx@outlook.com>
2024-04-03 18:17:34 -0600

    Adding first Dependency injection
    1. In this example, in the class Address, Salary and User, the annotation
    @Component was added
    
    2. We use the annotation @Autowired to create and inject an instance of
       User

--- spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
+++ spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
@@ -1,18 +1,24 @@
 package com.udemy.spring.springseleniumcourse;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringSeleniumCourseApplicationTests {
 
+    @Autowired
+    private User user;
+
     @Test
     void contextLoads() {
+        user.printDetails();
+    }
+    /*
         Address address = new Address();
         Salary salary = new Salary();
         User user = new User(address, salary);
         user.printDetails();
-
-    }
+     */
 
 }
