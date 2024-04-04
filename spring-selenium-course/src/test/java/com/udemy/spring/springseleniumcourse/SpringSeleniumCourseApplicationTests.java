5725fed19ccb696a9a464222557e7088ff3b35a6
AndresHGdeG <a.h.mx@outlook.com>
2024-04-03 20:30:28 -0600

    Dependency Injection @Value
    In this example, the  @Value annotation was used to inject values in the
    field level

--- spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
+++ spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
@@ -2,7 +2,10 @@ package com.udemy.spring.springseleniumcourse;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class SpringSeleniumCourseApplicationTests {
@@ -10,9 +13,21 @@ class SpringSeleniumCourseApplicationTests {
     @Autowired
     private User user;
 
+    @Value("${Path}")
+    private String path;
+
+    @Value("${fruits}")
+    private List<String> fruits;
+
+    @Value("${timeout}")
+    private int timeout;
+
     @Test
     void contextLoads() {
-        user.printDetails();
+        System.out.println(this.path);
+        System.out.println(this.timeout);
+        System.out.println(this.fruits);
+        System.out.println(this.fruits.size());
     }
 
 }
