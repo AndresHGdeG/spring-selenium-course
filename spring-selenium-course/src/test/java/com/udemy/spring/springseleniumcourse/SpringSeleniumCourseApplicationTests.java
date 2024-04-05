850218c574b04ba5c47f987b192e45a7e3806005
AndresHGdeG <a.h.mx@outlook.com>
2024-04-04 20:08:44 -0600

    Autowire An external Library
    In this example, the library java faker is used, but in
    order to create a dependency injection we need to create a configuration
    class with a @Configuration and @Bean annotation

--- spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
+++ spring-selenium-course/src/test/java/com/udemy/spring/springseleniumcourse/SpringSeleniumCourseApplicationTests.java
@@ -1,8 +1,8 @@
 package com.udemy.spring.springseleniumcourse;
 
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -13,26 +13,13 @@ class SpringSeleniumCourseApplicationTests {
     @Autowired
     private User user;
 
-    @Value("${URL:https://www.google.com}")
-    private String url;
-
-    @Value("${Path}")
-    private String path;
-
-    @Value("${fruits}")
-    private List<String> fruits;
-
-    @Value("${timeout}")
-    private int timeout;
+    @Autowired
+    private Faker fakerAuto;
 
     @Test
     void contextLoads() {
-        this.user.printDetails();
-        System.out.println(this.url);
-        System.out.println(this.path);
-        System.out.println(this.timeout);
-        System.out.println(this.fruits);
-        System.out.println(this.fruits.size());
+        System.out.println(this.fakerAuto.name().fullName());
+        user.printDetails();
     }
 
 }
