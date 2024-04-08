994891490700c0845c6734298874d73542a1fa0f
AndresHGdeG <a.h.mx@outlook.com>
2024-04-08 16:39:34 -0600

    Adding @ConditionalOnMissingBean
    The browser Microsoft Edge was added if case firefox and chrome is not
    select in the properties

--- src/main/java/com/udemy/spring/springseleniumcourse/config/WebDriverConfig.java
+++ src/main/java/com/udemy/spring/springseleniumcourse/config/WebDriverConfig.java
@@ -3,9 +3,11 @@ package com.udemy.spring.springseleniumcourse.config;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +34,13 @@ public class WebDriverConfig {
     public WebDriver fireFoxDriver(){
         WebDriverManager.firefoxdriver().setup();
         return new FirefoxDriver();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WebDriver edgeDriver(){
+        WebDriverManager.edgedriver().setup();
+        return new EdgeDriver();
     }
 
     @Bean
